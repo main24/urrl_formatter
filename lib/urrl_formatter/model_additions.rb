f06c73f2d7d2043596613599985df9f4e0e2d4eb
Valmir Dimas <valmirdimas@gmail.com>
2013-03-29 23:20:40 -0300

    Corrigindo nome da classe usada

--- lib/urrl_formatter/model_additions.rb
+++ lib/urrl_formatter/model_additions.rb
@@ -2,8 +2,9 @@ module UrrlFormatter
   module ModelAdditions
     def format_url(attribute)
       before_validation do
-        send("#{attribute}=", UrlFormatter.format_url(send(attribute)))
+        send("#{attribute}=", UrrlFormatter.format_url(send(attribute)))
       end
-      validates_format_of attribute, with: UrlFormatter.url_regexp, message: "is not a valid URL"    end
+      validates_format_of attribute, with: UrrlFormatter.url_regexp, message: "is not a valid URL"
+    end
   end
 end
