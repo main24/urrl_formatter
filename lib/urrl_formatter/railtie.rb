045f9aced4de9417f0cb62d0c105116b4dfbad00
Anton Pershakov <main24.fc@gmail.com>
2013-07-24 23:54:05 +0600

    Fix typo in calling of module ModelAdditions

--- lib/urrl_formatter/railtie.rb
+++ lib/urrl_formatter/railtie.rb
@@ -2,7 +2,7 @@ module UrrlFormatter
   class Railtie < Rails::Railtie
     initializer 'urrl_formatter.model_additions' do
       ActiveSupport.on_load :active_record do
-        extend ModelAddtions
+        extend ModelAdditions
       end
     end
   end
